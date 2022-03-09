9e7b5c75e8cec8d0f66e90216f9214b5c107397a
Wasi <48654021+wasitanbits@users.noreply.github.com>
2022-03-09 18:55:19 +0500

    chore: downgraded

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule Absinthe.Ecto.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:absinthe, "~> 1.6"},
+      {:absinthe, "~> 1.5"},
       {:ecto, ">= 0.0.0"},
       {:ex_doc, ">= 0.0.0", only: [:dev]},
       {:postgrex, ">= 0.16.2", only: [:test]},
